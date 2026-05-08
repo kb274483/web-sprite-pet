36572a29727b280cf0d1e77dde8b5b2d38f16ba6
Roy <kb274483@gmail.com>
2026-05-08 17:37:38 +0800

    fix:修正重複註冊tag name的問題

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,9 @@
 import { WebSpritePetElement } from './DesktopPetElement';
 
 const elementName = 'web-sprite-pet';
-const legacyElementName = 'desktop-pet';
 
 if (!customElements.get(elementName)) {
   customElements.define(elementName, WebSpritePetElement);
 }
 
-if (!customElements.get(legacyElementName)) {
-  customElements.define(legacyElementName, WebSpritePetElement);
-}
-
-export { WebSpritePetElement, WebSpritePetElement as DesktopPetElement };
+export { WebSpritePetElement };
